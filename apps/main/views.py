0ea5c880ce50bcab0ca20e4acfb7feceab413a33
hossein-sakkaki <sakkaki.hossein@gmail.com>
2023-05-07 11:57:35 +0300

    create index page & update setting

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-# from django
+from django.conf import settings
 # Create your views here.
+
+def media_admin(request):
+    return {'media_url': settings.MEDIA_URL}
 
 def index(request):
     return render(request, 'main_app/index.html')
