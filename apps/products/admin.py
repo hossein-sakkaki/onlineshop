3b26bd3fc86a39bd53e43a7895d9d624ee682e06
hossein-sakkaki <sakkaki.hossein@gmail.com>
2023-05-11 22:58:53 +0300

    update product admin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -7,7 +7,7 @@ from django.core import serializers
 from .models import Brand, ProductGroup, Feature, Product, ProductFeature, ProductGallery
 from django.db.models.aggregates import Count
 from django_admin_listfilter_dropdown.filters import DropdownFilter
-
+from django.db.models import Q
 
 #---------==========================================================---------#
 @admin.register(Brand)
@@ -25,9 +25,9 @@ def deactive_product_group(modeladmin, request, queryset):
     qs = queryset.update(is_active=False)
     message = ''
     if qs == 1:
-        message = f'{qs} item has been deactivated'
+        message = f'{qs} product group has been deactivated'
     elif qs > 1:
-        message = f'{qs} items were deactivated'
+        message = f'{qs} product groups were deactivated'
     modeladmin.message_user(request,message)
 
 def active_product_group(modeladmin, request, queryset):
@@ -68,5 +68,76 @@ class ProductGroupAdmin(admin.ModelAdmin):
         return obj.sub_group
     
     # count_sub_group.short_description = 'Count Sub Group'
+    
+#---------==========================================================---------#
+@admin.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ('feature_name',)
+    list_filter = ('feature_name',)
+    search_fields = ('feature_name',)
+    ordering = ('feature_name',)
+    
+#---------==========================================================---------#
+# Actions for product group admin #
+def deactive_product(modeladmin, request, queryset):
+    qs = queryset.update(is_active=False)
+    message = ''
+    if qs == 1:
+        message = f'{qs} product has been deactivated'
+    elif qs > 1:
+        message = f'{qs} products were deactivated'
+    modeladmin.message_user(request,message)
 
-     
+def active_product(modeladmin, request, queryset):
+    qs = queryset.update(is_active=True)
+    message = ''
+    if qs == 1:
+        message = f'{qs} item has been Activated'
+    elif qs > 1:
+        message = f'{qs} items were Activated'
+    modeladmin.message_user(request,message)
+
+# inline view product admin #
+class ProductFeatureInlineAdmin(admin.TabularInline):
+    model = ProductFeature
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name','display_product_group','price','brand','is_active','update_date','slug')
+    list_filter = ('brand','product_group')
+    search_fields = ('product_name',)
+    ordering = ('update_date','product_name')
+    prepopulated_fields = {'slug':('product_name','brand')}
+    
+    actions = [deactive_product, active_product]
+    inlines = [ProductFeatureInlineAdmin]
+
+    def display_product_group(self, obj):
+        return ', '.join([group.group_title for group in obj.product_group.all()])
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == 'product_group':
+            kwargs['queryset'] = ProductGroup.objects.filter(~Q(group_parent=None))
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+    
+    fieldsets = (
+        ('Product information', {
+            "fields": (
+                'product_name',
+                'product_group',
+                ('image_name','brand','is_active'),
+                'description',
+                'slug'
+            ),
+        }),
+        ('Date & Time', {
+            "fields": (
+                'published_date',
+            ),
+        }),
+    )
+    
+    
+    
+    
+#---------==========================================================---------#
