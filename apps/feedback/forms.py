ebc228aae6a99294d2d8f437106a8459bda28670
hossein-sakkaki <sakkaki.hossein@gmail.com>
2023-07-03 23:59:58 +0300

    forms of feesback

---
+++ apps/feedback/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class CommentForm(forms.Form):
+    product_id = forms.CharField(widget=forms.HiddenInput(), required=False)
+    comment_id = forms.CharField(widget=forms.HiddenInput(), required=False)
+    comment_text = forms.CharField(label='',
+                                   error_messages='requierd':'Field is requierd,
+                                   widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Comment text','rows':'4'}))
