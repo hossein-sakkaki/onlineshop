3cf10994165fde798e84879433263cc2a1178cea
hossein-sakkaki <sakkaki.hossein@gmail.com>
2023-07-04 23:42:35 +0300

    comment view

--- apps/feedback/forms.py
+++ apps/feedback/forms.py
@@ -4,5 +4,5 @@ class CommentForm(forms.Form):
     product_id = forms.CharField(widget=forms.HiddenInput(), required=False)
     comment_id = forms.CharField(widget=forms.HiddenInput(), required=False)
     comment_text = forms.CharField(label='',
-                                   error_messages='requierd':'Field is requierd,
+                                   error_messages={'requierd':'Field is requierd'},
                                    widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Comment text','rows':'4'}))
--- apps/feedback/views.py
+++ apps/feedback/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from .forms import CommentForm
+from apps.products.models import Product
+from .models import Comment
+from django.contrib import messages
 
-# Create your views here.
+class CommentView(View):
+    def get(self, requset, *args, **kwargs):
+        produst_id = requset.GET.get['productId']
+        comment_id = requset.GET.get['cpmmentId']
+        slug = kwargs['slug']
+        initial_dict = {
+            'produst_id': produst_id,
+            'comment_id': comment_id
+        }
+        form = CommentForm(initial=initial_dict)
+        return render(requset,'',{'form':form, 'slug': slug})
+    
+    def post(self, request, *args, **kwargs):
+        slug = kwargs['slug']
+        form = CommentForm(request.POST)
+        if form.is_valid:
+            data = form.cleaned_data
+            product = get_object_or_404(Product, slug= slug)
+            parent = None
+            if (data['comment_id']):
+                parent_id = data['comment_id']
+                parent = Comment.objects.get(id=parent_id)
+            Comment.objects.create(
+                product = product,
+                commenting_user = request.user,
+                comment_text = data['comment_text'],
+                comment_parent = parent
+            )
+            messages.success(request, 'Your message submit','success')
+            return redirect('products:product_detail', product.slug)
+        messages.error(request, 'Send error','danger')
+        return redirect('products:product_detail', product.slug)
+
+            
