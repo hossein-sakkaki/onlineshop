0c3c941e7afcaf3d7c2d5d44f7cda9aa7437d7ef
hossein-sakkaki <sakkaki.hossein@gmail.com>
2023-07-17 18:49:56 +0300

    update filter view

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('product_detail/<slug:slug>/',views.ProductDetailView.as_view(), name='product_detail'),
     path('product_groups/',views.ProductGroupsView.as_view(), name='product_groups'),
     path('product_by_groups/<slug:slug>/',views.ProductByGroup.as_view(), name='product_by_groups'),
-    # path('product_groups_partials/', views.get_product_groups, name='product_groups_partials'),
+    path('product_groups_partials/', views.get_product_groups, name='product_groups_partials'),
     
     path('compare_table/',views.compare_table, name='compare_table'),
     path('add_to_compare_list/',views.add_to_compare_list, name='add_to_compare_list'),
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.views import View
-from .models import Product, ProductGroup
+from .models import Product, ProductGroup, Brand
 from django.db.models import Q, Count, Min, Max
 from .compare import CompareProduct
+from django.core.paginator import Paginator
 # Create your views here.
 
-
-
 def get_root_group():
-    return ProductGroup.objects.filter(Q(is_active=True), Q(group_parent=None))
-
+    return ProductGroup.objects.filter(Q(is_active=True) & Q(group_parent=None))
 
 
 # Cheapest product
@@ -53,9 +51,6 @@ def get_related_product(request, *args, **kwargs):
 
 
 
-
-
-
 # detail product
 class ProductDetailView(View):
     def get(self, request, slug):
@@ -71,23 +66,99 @@ class ProductGroupsView(View):
                                         .order_by('-count')
         return render(request, 'products_app/product_groups.html', {'product_groups': product_groups})
 
+# product group list for filter
+def get_product_groups(request):
+    product_groups = ProductGroup.objects.annotate(count = Count('products_of_groups'))\
+                                        .filter(Q(is_active=True), ~Q(count = 0))\
+                                        .order_by('-count')
+    return render(request, 'products_app/partials/product_groups.html', {'product_groups': product_groups})
+
+# brands list for filter
+def get_brands(request, *args, **kwargs):
+    product_group = get_object_or_404(ProductGroup, slug=kwargs['slug'])
+    brand_list_id = product_group.products_of_groups.filter(is_active = True).values('brand_id')
+    brand = Brand.objects.filter(pk__in=brand_list_id)\
+                                .annotate(count = Count('products_of_brands'))\
+                                .filter(~Q(count=0))\
+                                .order_by('-count')
+    return render(request, 'product_app/partials/brands.html', {'brand': brand})
+
+def get_features_for_filter(request, *args, **kwargs):
+    product_group = get_object_or_404(ProductGroup, slug=kwargs['slug'])
+    feature_list = product_group.features_of_groups.all()
+    feature_dict = dict()
+    for feature in feature_list:
+        feature_dict[feature] = feature.feature_values.all()
+        
+    return render(request, 'product_app/partials/features_filter.html', {'feature_dict': feature_dict})
 
 
-
-# def get_product_groups(request):
-#     product_groups = ProductGroup.objects.annotate(count = Count('products_of_groups'))\
-#                                         .filter(Q(is_active=True), ~Q(count = 0))\
-#                                         .order_by('-count')
-#     return render(request, 'products_app/partials/product_groups.html', {'product_groups': product_groups})
+    
 
 
+class ProductsByGroupsView(View):
+    def get(self, request, *args, **kwargs):
+        slug = kwargs['slug']
+        current_group = get_object_or_404(ProductGroup, slug=slug)
+        products = Product.objects.filter(Q(is_active=True) & Q(product_group=current_group))
+        
+        res_aggr = products.aggregate(min=Min('price'), max=Max('price'))
+        
+        # price filter
+        filter = ProductFilter(request.GET, queryset=products)
+        products = filter.qs
+        
+        # brand filter
+        brands_filter = request.GET.getlist('brand')
+        if brands_filter:
+            products = products.filter(brand__id__in = brands_filter)
+            
+        # features filter
+        features_filter = request.GET.getlist('features')
+        if features_filter:
+            products = products.filter(product_feature__filter_value__id__in=features_filter).discard()
+            
+        sort_type = request.GET.get('sort_type')
+        if not sort_type:
+            sort_type = '0'
 
+        if sort_type == '1':
+            products = products.order_by('price')
+            
+        if sort_type == '2':
+            products = products.order_by('-price')
+            
+        group_slug = slug
+        product_per_page = 3
+        paginator = Paginator(products, product_per_page)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        product_count = products.count()
+        
+        show_count_product = []
+        i = product_per_page
+        while i < product_count:
+            show_count_product.append(i)
+            i *= 2
+        show_count_product.append(i)
+        
+        context = {
+            'products': products,
+            'current_group': current_group,
+            'res_aggr': res_aggr,
+            'group_slug': group_slug,
+            'page_obj': page_obj,
+            'product_count': product_count,
+            'show_count_product': show_count_product,
+            'filter': filter,
+            'sort_type': sort_type
+        }
+        return render(request, 'products_app/products_by_group.html', context)
+        
 
-
-
-
-
-
+            
+        
+            
 
 
 # All product of list groups
